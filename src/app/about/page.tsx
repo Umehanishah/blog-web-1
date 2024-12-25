45b96250bd9ef3962477e9a9c3fb3a1446f228bb
Umehani <missumehani@gmail.com>
2024-12-25 19:43:58 +0500

    updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -74,15 +74,15 @@ export default function About() {
                     0 Views <span className="px-5"> 0 Comments</span>
              
               {products.map((product) => (
-            <ul>
-            <li className="flex items-center text-lg">
+            
+            <div className="flex items-center text-lg">
                 <Button className="bg-transparent shadow-none hover:bg-transparent"
                   onClick={() => increaseQuantity(product.id)}
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="red" stroke="#ff0000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-heart hover:fill-red-600"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/></svg>
                 </Button>
                 {product.quantity}
-              </li></ul>
+              </div>
           ))}
                 </p>
                 </div>
